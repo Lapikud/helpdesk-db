28791fdd6aba7645f83cab559b4224be11e5385b
Mark Perman <maperm@taltech.ee>
2026-04-12 22:03:05 +0300

    refactor: clean up user assets from frontend

--- helpdeskdb-frontend/src/services/UserAssetsService.ts
+++
@@ -1,9 +0,0 @@
-
-import { EntityService } from "./EntityService";
-import { IUserAsset, IUserAssetAdd } from "@/types/domain/DomainTypes";
-
-export class UserAssetsService extends EntityService<IUserAsset, IUserAssetAdd> {
-	constructor(){
-		super('userassets')
-	}
-}
