01e52b624c5b56dcc61075fde71d0b9c472f8e0d
Mark Perman <maperm@taltech.ee>
2026-05-12 22:52:24 +0300

    fix: admin dropdown menu wasnt closing when clicked on asset reservations

--- helpdeskdb-frontend/src/components/Header.tsx
+++ helpdeskdb-frontend/src/components/Header.tsx
@@ -180,7 +180,7 @@ export default function Header() {
 										href="/assetReservations"
 										className="block px-4 py-2 text-sm hover:bg-gray-100 text-gray-700"
 										onClick={() =>
-											setIsUserDropdownOpen(false)
+											setIsAdminDropdownOpen(false)
 										}
 									>
 										{tLayout("AssetReservations")}
